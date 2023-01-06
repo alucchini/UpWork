ee9691502228a322364824d7b5db0ff17947634e
Antoine Lucchini <manokist@MacBook-Pro-de-Antoine.local>
2023-01-06 16:01:47 +0100

    test communication fingerprint

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,78 @@
 #include <Arduino.h>
+#include <Adafruit_Fingerprint.h>
 
-#define SERIAL_USB Serial
-#define SERIAL_FINGER Serial2
+#if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
+// For UNO and others without hardware serial, we must use software serial...
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+// Set up the serial port to use softwareserial..
+SoftwareSerial mySerial(2, 3); // RX, TX
+ 
+#else
+// On Leonardo/M0/etc, others with hardware serial, use hardware serial!
+// #0 is green wire, #1 is white
+#define mySerial Serial2
+ 
+#endif
+ 
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+ 
+uint8_t id;
 
-void setup()
-{
-  // start serial port at 9600 bps:
-  SERIAL_USB.begin(115200);
-  SERIAL_FINGER.begin(115200);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Serial.println("\n\nAdafruit Fingerprint sensor enrollment");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+
+  Serial.println(F("Reading sensor parameters"));
+  finger.getParameters();
+  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
+  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
+  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
+  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
+  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
+  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
+  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
+
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+
+  finger.getTemplateCount();
+
+  if (finger.templateCount == 0) {
+    Serial.println("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
+  }
+  else {
+    Serial.println("Waiting for valid finger...");
+    Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+  }
 }
 
-void loop()
-{
-  if (SERIAL_USB.available() > 0) {
-    SERIAL_FINGER.write(SERIAL_USB.read());
-  }
-  if (SERIAL_FINGER.available() > 0) {
-    SERIAL_USB.write(SERIAL_FINGER.read());
+void loop() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      // return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      // return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      // return p;
+    default:
+      Serial.println("Unknown error");
+      // return p;
   }
 }
