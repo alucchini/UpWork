6f04bfd67712c05a41de446fa64e65f9a752d193
Antoine Lucchini <manokist@MacBook-Pro-de-Antoine.local>
2023-01-27 12:30:00 +0100

    rebase

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,33 @@
 #include <HardwareSerial.h>
 #include "FingerprintManager.h"
 #include "SettingsManager.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "SER@YNOV";
+const char* password = "SteGYdUR227NtpEXT";
+
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";
+const char* mqtt_server = "10.33.10.193";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+//uncomment the following lines if you're using SPI
+/*#include <SPI.h>
+#define BME_SCK 18
+#define BME_MISO 19
+#define BME_MOSI 23
+#define BME_CS 5*/
+
+// LED Pin
+const int ledPin = 4;
 
 #if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
 // For UNO and others without hardware serial, we must use software serial...
@@ -24,6 +51,79 @@ uint8_t id;
 Match lastMatch;
 SettingsManager settingsManager;
 bool matchFound = false;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    wl_status_t wifi_status = WiFi.status();
+    Serial.print("WiFi status: ");
+    Serial.print(wifi_status);
+    delay(500);
+    Serial.print(". ");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 bool doPairing() {
   String newPairingCode = settingsManager.generateNewPairingCode();
@@ -123,9 +223,33 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   fingerManager.connect();
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+    // Convert the value to a char array
+    // char tempString[8];
+    // dtostrf(temperature, 1, 2, tempString);
+    // Serial.print("Temperature: ");
+    // Serial.println(tempString);
+    // client.publish("esp32/temperature", tempString);
+  }
+  sleep(2);
+
   Serial.println(" ");
   Serial.println(" ");
   Serial.println("Place finger on sensor");
@@ -148,5 +272,4 @@ void loop() {
     Serial.println(id);
     fingerManager.enrollFinger(id, "Test");
   }
-  sleep(2);
 }
